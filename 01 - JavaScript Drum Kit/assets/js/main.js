861d58021dc7e18db7980c4cf30a378b5cf1fcfc
George Kuo <ridnour@gmail.com>
2018-07-25 17:40:17 +0800

    keyboard can play

---
+++ 01 - JavaScript Drum Kit/assets/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    audio.currentTime = 0; //每次 keydown 時，audio 回到第0秒
+    audio.play();
+    key.classList.toggle('playing');
+
+})
--- 01 - JavaScript Drum Kit/readme.md
+++ 01 - JavaScript Drum Kit/readme.md
@@ -1,3 +1,5 @@
 # 參考連結
 
 <a href="https://pjchender.blogspot.com/2017/01/javascript-es6-template-literalstagged.html" target="_blank">[筆記] JavaScript ES6 中的模版字符串（template literals）和標籤模版（tagged template）</a>
+
+<a href="https://developer.mozilla.org/zh-TW/docs/Web/API/Element/classList" target="_blank">Element.classList</a>
