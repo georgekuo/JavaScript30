1ac7b5aa856b46568b76f5ae771b9e87cf1e2ff8
KuoGeorge <ridnour@gmail.com>
2018-07-26 14:30:06 +0800

    優化soundPlay function

--- 01 - JavaScript Drum Kit/assets/js/main.js
+++ 01 - JavaScript Drum Kit/assets/js/main.js
@@ -1,11 +1,12 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    if(!audio) return;
-    audio.currentTime = 0; //每次 keydown 時，audio 回到第0秒
-    audio.play();
-    key.classList.add('playing');
-})
+
+function soundPlay(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return;
+  audio.currentTime = 0; //每次 keydown 時，audio 回到第0秒
+  audio.play();
+  key.classList.add('playing');
+}
 
 function removeTransition(e) { 
   //console.log(e) //TransitionEvent
@@ -15,3 +16,5 @@ function removeTransition(e) {
 }
 const keys = document.querySelectorAll('.key'); //取得 keys 的 node list
 keys.forEach(key => key.addEventListener("transitionend", removeTransition)); //讓 keys 裡每個 div 動畫結束後，馬上移除動畫
+
+window.addEventListener('keydown', soundPlay)
